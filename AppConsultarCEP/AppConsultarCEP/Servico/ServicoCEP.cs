2f12168126de8eb4f4960687602646bd795d1169
Marciano Pasa <marcianoap@hotmail.com>
2020-11-07 13:40:43 -0300

    Add project files.

---
+++ AppConsultarCEP/AppConsultarCEP/Servico/ServicoCEP.cs
@@ -0,0 +1,31 @@
+﻿using AppConsultarCEP.Modelo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace AppConsultarCEP.Servico
+{
+    public class ServicoCEP
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoPeloCEP(string cep)
+        {
+            string novaURL = string.Format(EnderecoURL, cep);
+
+            WebClient webClient = new WebClient();
+            string retorno = webClient.DownloadString(novaURL);
+
+            Endereco endereco = JsonConvert.DeserializeObject<Endereco>(retorno);
+
+            if (endereco.cep == null)
+            {
+                return null;
+            }
+
+            return endereco;
+        }
+    }
+}
